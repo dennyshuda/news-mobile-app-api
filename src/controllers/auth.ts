346ba195409f22c1149a360b765ba0a81b394935
Denny Setyawan Huda <denycam9@gmail.com>
2024-06-07 03:06:26 +0700

    feat: add refresh token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma";
-import { signJWT } from "../utils/jwt";
+import { signJWT, verifyJWT } from "../utils/jwt";
 
 export const register = async (req: Request, res: Response) => {
 	const { username, email, password } = req.body;
+
+	if (username === "" || email === "" || password === "")
+		return res.status(400).json({ status: false, statusCode: 400, message: "Check your data" });
 
 	try {
 		const hashedPassword = await bcrypt.hash(password, 10);
@@ -17,9 +20,13 @@ export const register = async (req: Request, res: Response) => {
 			},
 		});
 
-		return res.status(201).json({ message: "user created successfully" });
+		return res
+			.status(201)
+			.json({ status: true, statusCode: 201, message: "User created successfully" });
 	} catch (error) {
-		return res.status(500).json({ message: "Failed to create user" });
+		return res
+			.status(500)
+			.json({ status: false, statusCode: 500, message: "Failed to create user" });
 	}
 };
 
@@ -30,20 +37,54 @@ export const login = async (req: Request, res: Response) => {
 		const user = await prisma.user.findUnique({
 			where: { email },
 		});
-
-		if (!user) return res.status(401).json({ message: "Invalid user" });
+		if (!user)
+			return res.status(500).json({ status: false, statuCode: 400, message: "User not found" });
 
 		const isPasswordMatch = await bcrypt.compare(password, user?.password!);
-
-		if (!isPasswordMatch) return res.status(401).json({ message: "Invalid credential" });
+		if (!isPasswordMatch)
+			return res
+				.status(400)
+				.json({ status: false, statusCode: 400, message: "Password not match" });
 
 		const { password: userPassword, ...userInfo } = user;
+		const accessToken = signJWT({ ...userInfo }, { expiresIn: "2h" });
+		const refreshToken = signJWT({ ...userInfo }, { expiresIn: "30d" });
 
-		const token = signJWT({ id: user.id }, { expiresIn: "15m" });
-
-		return res.status(200).json({ ...userInfo, token });
+		return res.status(200).json({
+			status: true,
+			statusCode: 200,
+			message: "Success to login",
+			...userInfo,
+			data: { accessToken: accessToken, refreshToken: refreshToken },
+		});
 	} catch (error) {
 		console.log(error);
-		return res.status(500).json({ message: "Failed to login" });
+		return res.status(500).json({ status: false, statusCode: 500, message: "Failed to login" });
+	}
+};
+
+export const refreshSession = async (req: Request, res: Response) => {
+	const { refreshToken } = req.body;
+
+	try {
+		const { decoded }: any = verifyJWT(refreshToken);
+		const user = await prisma.user.findUnique({ where: { email: decoded.email } });
+		if (!user)
+			return res.status(400).json({ status: false, statusCode: 400, message: "User not found" });
+
+		const { password, ...userInfo } = user;
+		const token = signJWT({ ...userInfo }, { expiresIn: "1d" });
+
+		return res.status(200).json({
+			status: true,
+			statusCode: 200,
+			message: "Success to generate access token",
+			accessToken: token,
+			...userInfo,
+		});
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ status: false, statusCode: 500, message: "Failed to generate access token" });
 	}
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { login, register } from "../controllers/auth";
+import { login, refreshSession, register } from "../controllers/auth";
 
 export const AuthRouter = express.Router();
 
 AuthRouter.post("/register", register);
 AuthRouter.post("/login", login);
+AuthRouter.post("/refresh", refreshSession);
